d40a7255145ba2b38282125d8386550dd1f7badc
Arseniy Shablyko <241208arseniy@gmail.com>
2025-02-27 18:30:18 +0300

    Added tests04

--- Task01/test03.cpp
+++ Task01/test03.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-using namespace std;
-
-int* test() {
-	int array[]{ 1,2,3,4,5,6,7,8,9,10 };
-	return array;
-}
-
-int main() {
-	int* pointer = test();
-
-	for (int i = 0; i < 10; i++) {
-		cout << *(pointer + i) << " ";
-	}
-
-	cout << endl;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int* test() {
+//	int array[]{ 1,2,3,4,5,6,7,8,9,10 };
+//	return array;
+//}
+//
+//int main() {
+//	int* pointer = test();
+//
+//	for (int i = 0; i < 10; i++) {
+//		cout << *(pointer + i) << " ";
+//	}
+//
+//	cout << endl;
+//
+//	return 0;
+//}
